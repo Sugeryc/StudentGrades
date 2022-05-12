a9a12f5fcd2957f3234552a037d6d27d942941bd
yc <1653077041@qq.com>
2022-05-12 11:15:16 +0800

    学生登录模块

--- src/main/java/com/score/service/TStudentService.java
+++ src/main/java/com/score/service/TStudentService.java
@@ -1,4 +1,10 @@
 package com.score.service;
 
+import com.score.bean.TStudent;
+
+import java.util.List;
+
 public interface TStudentService {
+    List<TStudent> selectloginStudent(TStudent student);
+
 }
